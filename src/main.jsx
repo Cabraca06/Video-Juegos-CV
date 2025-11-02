23108e1db99ffeab9909f67c5aba168a34eefce9
Richard cabraca <richardcabraca012@gmail.com>
2025-11-02 12:50:46 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from './App.jsx';
+import Login from "./assets/components/pages/Login.jsx";
 
 
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Login />
     </BrowserRouter>
   </React.StrictMode>
 );  
