6ba40236b43dcc6e5920e5cbbe0348e795d0bfd2
Richard cabraca <richardcabraca012@gmail.com>
2025-11-05 11:19:37 -0600

    build: Genera archivos para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Esta línea es clave para que funcione en GitHub Pages
   base: '/Video-Juegos-CV/',
-});
-
+  build: {
+    outDir: 'docs'
+  }
+})
