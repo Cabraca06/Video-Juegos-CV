861747ad80f7342041a89868401f6f2d6eb7a8e6
Richard cabraca <richardcabraca012@gmail.com>
2025-11-05 11:24:42 -0600

    Update consolasData.js

--- src/Data/consolasData.js
+++ src/Data/consolasData.js
@@ -3,10 +3,12 @@ import React from 'react';
 import ps5 from '../img/Consola PS5.webp';
 import xbox from '../img/XBOX series .webp'
 import nintendoSO from '../img/Nintendo switch oled.webp'
+import ps2 from '../img/PS2.webp'
 
 export const consolas = [
     {id:1, nombre: 'PlayStation 5', fabricante: 'Sony', imagen: ps5, descripcion: 'La PlayStation 5 es la consola de última generación de Sony, ofreciendo gráficos impresionantes y tiempos de carga rápidos.'},
     {id: 2, nombre: 'XboX Series X', fabricante: 'Microsoft', imagen:xbox, descripcion: 'La Xbox Series X es la consola más potente de Microsoft, diseñada para ofrecer una experiencia de juego fluida y de alta calidad.'},
     {id:3, nombre: 'Nintendo Switch', fabricante: 'Nintendo', imagen: nintendoSO, descripcion: 'La Nintendo Switch es una consola híbrida que permite jugar tanto en casa como en movimiento.'},
+    {id:4, nombre: 'PlayStation 2', fabricante: 'Sony', imagen: ps2, descripcion: 'La PlayStation 2 es una consola de videojuegos de Sony que ofrece una amplia gama de juegos y características multimedia.'},
 ];
 
